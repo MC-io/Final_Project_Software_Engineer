cd29eba9a71becfcf1e4fec07fd3950c07cd19fb
Moises <mcasaverde@unsa.edu.pe>
2022-08-18 17:47:02 -0500

    Changing Folders

--- Modelo Candidato/backend/models/asistente.py
+++ Modelo Candidato/backend/models/asistente.py
@@ -5,7 +5,7 @@ class AsistenteModel:
 
     def get_asistente(self, id):
         params = {'id': id}
-        rv = self.mysql_pool.execute("SELECT * from usuario INNER JOIN asistente ON where asistente.id%(id)s=usuario.id%(id)s", params)                
+        rv = self.mysql_pool.execute("SELECT * from usuario INNER JOIN asistente ON asistente.id%(id)s=usuario.id%(id)s", params)                
         data = []
         content = {}
         for result in rv:
@@ -14,11 +14,22 @@ class AsistenteModel:
             content = {}
         return data
     def get_all_asistentes(self):
-        rv = self.mysql_pool.execute("select * from usuario")
+        rv = self.mysql_pool.execute("select * from usuario order by id")
         data = []
         content = {}
-        
+        for result in rv:
+            content = {'id': result[0], 'nombre': result[1], 'apellido': result[2], 'correo': result[3]}
+            data.append(content)
+            content = {}
+        return data
 
     def create_asistente(self, id, nombre, apellido, correo):
-
-    def delete_asistente(self, id)
+        params = {
+            'id' : id,
+            'nombre' : nombre,
+            'apellido' : apellido,
+            'correo' : correo,
+        }  
+        
+    def delete_asistente(self, id):
+        
